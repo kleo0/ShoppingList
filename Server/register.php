9dbe1d83bd92cd50004d5d98591b52bc29a9ed36
sheadovas <sheadovas@gmail.com>
2017-11-08 21:31:22 +0100

    S: Fix Register, drop column public_name

--- Server/register.php
+++ Server/register.php
@@ -10,19 +10,16 @@ if(!$db_ok) {
   finish($res);
 }
 
-if(empty($_POST['nickname']) || empty($_POST['password']) || empty($_POST['public_name'])) {
+if(empty($_POST['nickname']) || empty($_POST['password'])) {
   $res['ERR'] = ERR_NOT_ALL_VARS_ARE_SET;
   finish($res);
 }
 
 $nickname = $_POST['nickname'];
 $password = $_POST['password'];
-$public_name = $_POST['public_name'];
 
 if(!isBetween(strlen($nickname), 1, MAX_LOGIN_LEN) ||
-    !isBetween(strlen($password), MIN_PASS_LEN, MAX_PASS_LEN) ||
-    !isBetween(strlen($public_name), 1, MAX_PUB_NAME_LEN)) {
-  print $nickname . $password . $public_name;
+    !isBetween(strlen($password), MIN_PASS_LEN, MAX_PASS_LEN)) {
 
   $res['ERR'] = ERR_UNMET_DEPENDENCIES;
   finish($res);
@@ -35,7 +32,7 @@ try {
   $sth->execute();
 
 } catch (Exception $e) {
-  die($e);
+  die($e->getMessage());
 }
 
 if($sth->rowCount() > 0) {
@@ -44,15 +41,14 @@ if($sth->rowCount() > 0) {
 }
 
 try {
-  $sth = $dbh->prepare('insert into users (nickname,password,public_name) ' .
-    'values (:nickname,:password,:public_name)');
+  $sth = $dbh->prepare('insert into users (nickname,password) ' .
+    'values (:nickname,:password)');
   $sth->bindParam(':nickname', $nickname, PDO::PARAM_STR, MAX_LOGIN_LEN);
   $sth->bindParam(':password', $password, PDO::PARAM_STR);
-  $sth->bindParam(':public_name', $public_name, PDO::PARAM_STR, MAX_PUB_NAME_LEN);
   $sth->execute();
 
 } catch (Exception $e) {
-  die($e);
+  die($e->getMessage());
 }
 
 finish($res);
