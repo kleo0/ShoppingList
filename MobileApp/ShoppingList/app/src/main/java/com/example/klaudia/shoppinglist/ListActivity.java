483bb2e9a8a98ba393094fb676a0ff96c8f3e535
Klaudia Rzonca <klaudia_rzonca@op.pl>
2017-11-22 20:52:23 +0100

    MA: add scan by camera && and save data to server

--- MobileApp/ShoppingList/app/src/main/java/com/example/klaudia/shoppinglist/ListActivity.java
+++ MobileApp/ShoppingList/app/src/main/java/com/example/klaudia/shoppinglist/ListActivity.java
@@ -1,18 +1,26 @@
 package com.example.klaudia.shoppinglist;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.style.UpdateAppearance;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.beardedhen.androidbootstrap.BootstrapButton;
@@ -20,9 +28,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
-import com.koushikdutta.ion.builder.Builders;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -38,6 +44,13 @@ public class ListActivity extends AppCompatActivity {
     ListView listView;
     StoreData storeData;
     ArrayList<Product> productList;
+    JsonArray deleteList;
+    JsonArray addList;
+
+    public static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
+    public static final String ALLOW_KEY = "ALLOWED";
+    public static final String CAMERA_PREF = "camera_pref";
+    private static final int CAMERA_REQUEST = 1888;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +60,8 @@ public class ListActivity extends AppCompatActivity {
         Intent i = getIntent();
         lid = i.getStringExtra("lid");
         name = i.getStringExtra("name");
+        deleteList = new JsonArray();
+        addList = new JsonArray();
 
         storeData = new StoreData(this);
         CheckButton();
@@ -94,6 +109,13 @@ public class ListActivity extends AppCompatActivity {
                     Product product = productList2.get(i);
                     if (!product.isSelected()) {
                         template.add(product);
+                    } else {
+                        //save data to send to server
+                        JsonObject obj = new JsonObject();
+                        String[] prod = product.getName().split(" ");
+                        obj.addProperty("n",prod[0]);
+                        obj.addProperty("q",prod[1]);
+                        deleteList.add(obj);
                     }
                 }
                 dataAdapter.ProductList.clear();
@@ -132,8 +154,10 @@ public class ListActivity extends AppCompatActivity {
 
                                     JsonArray products = array.get("products").getAsJsonArray();
                                     for (int i = 0; i < products.size(); i++) {
-                                        Product product = new Product(products.get(i).getAsJsonObject().get("n") + " " +
-                                                products.get(i).getAsJsonObject().get("q"), false);
+                                        String entry = products.get(i).getAsJsonObject().get("n") + " " +
+                                                products.get(i).getAsJsonObject().get("q");
+                                        entry = entry.replace("\"", "");
+                                        Product product = new Product(entry, false);
                                         productList.add(product);
                                     }
                                     DisplayList();
@@ -193,6 +217,13 @@ public class ListActivity extends AppCompatActivity {
         String[] p = input.split(" ");
         Product product = new Product(p[0]+ " " + p[1], false);
         productList2.add(product);
+
+        //save data to send to server
+        JsonObject obj = new JsonObject();
+        obj.addProperty("n",p[0]);
+        obj.addProperty("q",p[1]);
+        addList.add(obj);
+
         dataAdapter.ProductList.clear();
         productList.clear();
         productList.addAll(productList2);
@@ -206,6 +237,37 @@ public class ListActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+                JsonObject obj = new JsonObject();
+                obj.addProperty("list_id",lid);
+                obj.add("add",addList);
+                obj.add("del",deleteList);
+                Log.d("HELLO",obj.toString());
+                try {
+                    Ion.with(getApplicationContext())
+                            .load("http://skyapplab.duckdns.org:7777/list.php")
+                            .setBodyParameter("token", URLEncoder.encode(storeData.GetToken(), "UTF-8"))
+                            .setBodyParameter("action", "mod")
+                            .setBodyParameter("data", obj.toString())
+                            .asJsonObject()
+                            .setCallback(new FutureCallback<JsonObject>() {
+                                @Override
+                                public void onCompleted(Exception e, JsonObject result) {
+                                    if (result != null) {
+                                        if (result.get("ERR").toString().equals("0")) {
+                                            //czy lista zapisana?
+                                            Toast.makeText(getApplicationContext(),"Data seved!",Toast.LENGTH_LONG).show();
+                                        } else {
+                                            Toast.makeText(getApplicationContext(), "Send data error!"
+                                                    + result.get("ERR").toString(), Toast.LENGTH_LONG).show();
+                                        }
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "Check internet connection!", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
             }
 
         });
@@ -217,8 +279,151 @@ public class ListActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(ListActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    if (getFromPref(ListActivity.this, ALLOW_KEY)) {
+                        showSettingsAlert();
+                    } else if (ContextCompat.checkSelfPermission(ListActivity.this,
+                            Manifest.permission.CAMERA)
+
+                            != PackageManager.PERMISSION_GRANTED) {
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(ListActivity.this,
+                                Manifest.permission.CAMERA)) {
+                            showAlert();
+                        } else {
+                            ActivityCompat.requestPermissions(ListActivity.this,
+                                    new String[]{Manifest.permission.CAMERA},
+                                    MY_PERMISSIONS_REQUEST_CAMERA);
+                        }
+                    }
+                } else {
+                    openCamera();
+                }
             }
 
         });
+    }
+
+    public static void saveToPreferences(Context context, String key, Boolean allowed) {
+        SharedPreferences myPrefs = context.getSharedPreferences(CAMERA_PREF,
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = myPrefs.edit();
+        prefsEditor.putBoolean(key, allowed);
+        prefsEditor.apply();
+    }
+
+    public static Boolean getFromPref(Context context, String key) {
+        SharedPreferences myPrefs = context.getSharedPreferences(CAMERA_PREF,
+                Context.MODE_PRIVATE);
+        return (myPrefs.getBoolean(key, false));
+    }
+
+    private void showAlert() {
+        AlertDialog alertDialog = new AlertDialog.Builder(ListActivity.this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage("App needs to access the Camera.");
+
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "DONT ALLOW",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        finish();
+                    }
+                });
+
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "ALLOW",
+                new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        ActivityCompat.requestPermissions(ListActivity.this,
+                                new String[]{Manifest.permission.CAMERA},
+                                MY_PERMISSIONS_REQUEST_CAMERA);
+                    }
+                });
+        alertDialog.show();
+    }
+
+    private void showSettingsAlert() {
+        AlertDialog alertDialog = new AlertDialog.Builder(ListActivity.this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage("App needs to access the Camera.");
+
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "DONT ALLOW",
+                new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        //finish();
+                    }
+                });
+
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "SETTINGS",
+                new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        startInstalledAppDetailsActivity(ListActivity.this);
+                    }
+                });
+
+        alertDialog.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                for (int i = 0, len = permissions.length; i < len; i++) {
+                    String permission = permissions[i];
+
+                    if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                        boolean
+                                showRationale =
+                                ActivityCompat.shouldShowRequestPermissionRationale(
+                                        this, permission);
+
+                        if (showRationale) {
+                            showAlert();
+                        } else if (!showRationale) {
+                            saveToPreferences(ListActivity.this, ALLOW_KEY, true);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    public static void startInstalledAppDetailsActivity(final Activity context) {
+        if (context == null) {
+            return;
+        }
+
+        final Intent i = new Intent();
+        i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        i.addCategory(Intent.CATEGORY_DEFAULT);
+        i.setData(Uri.parse("package:" + context.getPackageName()));
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+        context.startActivity(i);
+    }
+
+    private void openCamera() {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(intent, CAMERA_REQUEST);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            //imageView.setImageBitmap(photo);
+            //TODO send picture, check list
+        }
     }
 }
